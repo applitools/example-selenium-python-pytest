5e583fc02e089fdbd10b739238c465bb09f7f212
Serhii Khalymon <sergiykhalimon@gmail.com>
2019-05-29 13:29:28 +0300

    Add close_async and get_all_test_results to tutorial

--- visual_grid_demo.py
+++ visual_grid_demo.py
@@ -2,14 +2,20 @@ from webdriver_manager.chrome import ChromeDriverManager
 from selenium.webdriver import Chrome
 
 from applitools.selenium import (
-    VisualGridRunner, Eyes, Target, Configuration, BatchInfo, BrowserType, DeviceName,
+    VisualGridRunner,
+    Eyes,
+    Target,
+    Configuration,
+    BatchInfo,
+    BrowserType,
+    DeviceName,
 )
 
 
 def initialize_eyes(runner):
     eyes = Eyes(runner)
     # Set API key
-    eyes.api_key = 'YOU API KEY'
+    eyes.api_key = "YOU API KEY"
 
     # If dedicated or on-prem cloud, uncomment and enter the cloud url
     # Default: https://eyes.applitools.com
@@ -19,7 +25,7 @@ def initialize_eyes(runner):
     sconf = Configuration()
 
     # Set the AUT name
-    sconf.app_name = 'Blank App'
+    sconf.app_name = "Blank App"
 
     # Set a test name
     sconf.test_name = "Smoke Test via Visual Grid"
@@ -55,10 +61,10 @@ def run_test():
     driver = Chrome(ChromeDriverManager().install())
 
     # Navigate to the URL we want to test
-    # driver.get("https://demo.applitools.com")
+    driver.get("https://demo.applitools.com")
 
     # To see visual bugs, change the above URL to:
-    driver.get("https://demo.applitools.com/index_v2.html")
+    # driver.get("https://demo.applitools.com/index_v2.html")
     # https://demo.applitools.com/index_v2.html and run the test again
 
     # Call Open on eyes to initialize a test session
@@ -68,7 +74,7 @@ def run_test():
     eyes.check("Step 1 - Login page", Target.window().fully())
 
     # Click on the Login button to go to the App's main page
-    driver.find_element_by_id('log-in').click()
+    driver.find_element_by_id("log-in").click()
 
     # Check the App page
     eyes.check("Step 2 - App Page", Target.window().fully())
@@ -77,11 +83,20 @@ def run_test():
     print(
         "Please wait... we are now: \n1. Uploading resources, \n2. Rendering in Visual Grid, "
         "and \n3. Using Applitools A.I. to validate the checkpoints. \nIt'll take about 30 "
-        "secs to a minute...")
+        "secs to a minute..."
+    )
 
     # Close eyes and collect results
-    eyes.close()
+    # This could be used instead of close_async and get_all_test_results
+    # eyes.close()
+
+    # Send close task
+    eyes.close_async()
+
+    # Return test results from all eyes instances
+    all_test_results = runner.get_all_test_results()
+    print(all_test_results)
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     run_test()
