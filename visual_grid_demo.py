a4accc39bb2ae2762c1b087e072815270de1f94e
Serhii Khalymon <sergiykhalimon@gmail.com>
2019-12-16 17:29:08 +0200

    Update tutorial to match 4.1.x

--- visual_grid_demo.py
+++ visual_grid_demo.py
@@ -2,6 +2,7 @@ from webdriver_manager.chrome import ChromeDriverManager
 from selenium.webdriver import Chrome
 
 from applitools.selenium import (
+    logger,
     VisualGridRunner,
     Eyes,
     Target,
@@ -11,61 +12,33 @@ from applitools.selenium import (
     DeviceName,
 )
 
+# Create a runner with concurrency of 10
+visual_grid_runner = VisualGridRunner(10)
+# Initialize Eyes with Visual Grid Runner
+eyes = Eyes(visual_grid_runner)
 
-def initialize_eyes(runner):
-    eyes = Eyes(runner)
-    # Set API key
-    eyes.api_key = "YOU API KEY"
+# Create a new Webdriver, ChromeDriverManager is uses for detect or download the chromedriver
+driver = Chrome(ChromeDriverManager().install())
+logger.set_logger(logger.StdoutLogger())
 
-    # If dedicated or on-prem cloud, uncomment and enter the cloud url
-    # Default: https://eyes.applitools.com
-    # eyes.server_url = "https://testeyes.applitools.com"
+# Create SeleniumConfiguration.
+conf = (Configuration()
+        .set_api_key("YOU API KEY")
+        .set_app_name("Blank App")
+        .set_test_name("Smoke Test via Visual Grid")
+        .set_batch(BatchInfo("VIP Browser combo batch"))
+        .add_browser(800, 600, BrowserType.CHROME)
+        .add_browser(700, 500, BrowserType.CHROME)
+        .add_browser(1200, 800, BrowserType.FIREFOX)
+        .add_browser(1600, 1200, BrowserType.FIREFOX)
+        .add_device_emulation(DeviceName.iPhone_4))
 
-    # Create SeleniumConfiguration.
-    sconf = Configuration()
+# Set the configuration object to eyes
+eyes.set_configuration(conf)
 
-    # Set the AUT name
-    sconf.app_name = "Blank App"
-
-    # Set a test name
-    sconf.test_name = "Smoke Test via Visual Grid"
-
-    #  Set a batch name so all the different browser and mobile combinations are
-    #  part of the same batch
-    sconf.batch = BatchInfo("VIP Browser combo batch")
-
-    # Add Chrome browsers with different Viewports
-    sconf.add_browser(800, 600, BrowserType.CHROME)
-    sconf.add_browser(700, 500, BrowserType.CHROME)
-
-    # Add Firefox browser with different Viewports
-    sconf.add_browser(1200, 800, BrowserType.FIREFOX)
-    sconf.add_browser(1600, 1200, BrowserType.FIREFOX)
-
-    # Add iPhone 4 device emulation
-    sconf.add_device_emulation(DeviceName.iPhone_4)
-
-    # Set the configuration object to eyes
-    eyes.configuration = sconf
-    return eyes
-
-
-def run_test():
-    # Create a runner with concurrency of 10
-    runner = VisualGridRunner(10)
-
-    # Initialize Eyes with Visual Grid Runner
-    eyes = initialize_eyes(runner)
-
-    # Create a new Webdriver, ChromeDriverManager is uses for detect or download the chromedriver
-    driver = Chrome(ChromeDriverManager().install())
-
+try:
     # Navigate to the URL we want to test
     driver.get("https://demo.applitools.com")
-
-    # To see visual bugs, change the above URL to:
-    # driver.get("https://demo.applitools.com/index_v2.html")
-    # https://demo.applitools.com/index_v2.html and run the test again
 
     # Call Open on eyes to initialize a test session
     eyes.open(driver)
@@ -79,24 +52,19 @@ def run_test():
     # Check the App page
     eyes.check("Step 2 - App Page", Target.window().fully())
 
-    driver.quit()
     print(
         "Please wait... we are now: \n1. Uploading resources, \n2. Rendering in Visual Grid, "
         "and \n3. Using Applitools A.I. to validate the checkpoints. \nIt'll take about 30 "
-        "secs to a minute..."
-    )
-
-    # Close eyes and collect results
-    # This could be used instead of close_async and get_all_test_results
-    # eyes.close()
+        "secs to a minute...")
 
     # Send close task
     eyes.close_async()
-
-    # Return test results from all eyes instances
-    all_test_results = runner.get_all_test_results()
-    print(all_test_results)
-
-
-if __name__ == "__main__":
-    run_test()
+except Exception as e:
+    print(str(e))
+    # If the test was aborted before eyes.close / eyes.close_async was called, ends the test
+    # as aborted.
+    eyes.abort()
+finally:
+    driver.quit()
+    results = visual_grid_runner.get_all_test_results()
+    print(results)
