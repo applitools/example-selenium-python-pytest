e6ba7fa532bf283ce22dc53f03c8e522a27ba38e
artem <artem.borodavka@applitools.com>
2021-05-25 11:44:57 +0300

    Update UFG tutorial to use pytest as the basic one

--- ultrafastgrid_demo.py
+++ ultrafastgrid_demo.py
@@ -1,10 +1,9 @@
-from webdriver_manager.chrome import ChromeDriverManager
-from selenium.webdriver import Chrome
 import os
+import pytest
 
-
+from webdriver_manager.chrome import ChromeDriverManager
+from selenium import webdriver
 from applitools.selenium import (
-    logger,
     VisualGridRunner,
     Eyes,
     Target,
@@ -14,74 +13,82 @@ from applitools.selenium import (
 )
 
 
-def set_up(eyes):
-    # create a new batch info instance and set it to the configuration
-    eyes.configure.set_batch(BatchInfo("Ultrafast Batch"))
+@pytest.fixture(scope="module")
+def batch_info():
+    """
+    Use one BatchInfo for all tests inside module
+    """
+    return BatchInfo("Ultrafast Batch")
+
+
+@pytest.fixture(name="driver", scope="function")
+def driver_setup():
+    """
+    New browser instance per test and quite.
+    """
+    # Set chrome driver to headless when running on the CI
+    options = webdriver.ChromeOptions()
+    options.headless = (os.getenv('CI', 'False') == 'true')
+
+    driver = webdriver.Chrome(ChromeDriverManager().install(), options=options)
+    yield driver
+    # Close the browser.
+    driver.quit()
+
+
+@pytest.fixture(name="runner", scope="session")
+def runner_setup():
+    """
+    One test runner for all tests. Print test results in the end of execution.
+    """
+    runner = VisualGridRunner()
+    yield runner
+    all_test_results = runner.get_all_test_results()
+    print(all_test_results)
+
+
+@pytest.fixture(name="eyes", scope="function")
+def eyes_setup(runner, batch_info):
+    """
+    Basic Eyes setup. It'll abort test if wasn't closed properly.
+    """
+    eyes = Eyes(runner)
+    # Initialize the eyes SDK and set your private API key.
+    eyes.api_key = os.environ["APPLITOOLS_API_KEY"]
+    eyes.configure.batch = batch_info
 
     # Add browsers with different viewports
     # Add mobile emulation devices in Portrait mode
     (
         eyes.configure.add_browser(800, 600, BrowserType.CHROME)
-        .add_browser(700, 500, BrowserType.FIREFOX)
-        .add_browser(1600, 1200, BrowserType.IE_11)
-        .add_browser(1024, 768, BrowserType.EDGE_CHROMIUM)
-        .add_browser(800, 600, BrowserType.SAFARI)
-        .add_device_emulation(DeviceName.iPhone_X)
-        .add_device_emulation(DeviceName.Pixel_2)
+            .add_browser(700, 500, BrowserType.FIREFOX)
+            .add_browser(1600, 1200, BrowserType.IE_11)
+            .add_browser(1024, 768, BrowserType.EDGE_CHROMIUM)
+            .add_browser(800, 600, BrowserType.SAFARI)
+            .add_device_emulation(DeviceName.iPhone_X)
+            .add_device_emulation(DeviceName.Pixel_2)
     )
 
-
-def ultra_fast_test(web_driver, eyes):
-    try:
-        # Navigate to the url we want to test
-        web_driver.get("https://demo.applitools.com")
-
-        # Call Open on eyes to initialize a test session
-        eyes.open(
-            web_driver, "Demo App - python", "Ultrafast grid demo", {"width": 800, "height": 600}
-        )
-
-        # check the login page with fluent api, see more info here
-        # https://applitools.com/docs/topics/sdk/the-eyes-sdk-check-fluent-api.html
-        eyes.check("", Target.window().fully().with_name("Login page"))
-
-        web_driver.find_element_by_id("log-in").click()
-
-        # Check the app page
-        eyes.check("", Target.window().fully().with_name("App page"))
-
-        # Call Close on eyes to let the server know it should display the results
-        eyes.close_async()
-    except Exception as e:
-        eyes.abort_async()
-        print(e)
+    yield eyes
+    # If the test was aborted before eyes.close was called, ends the test as aborted.
+    eyes.abort_if_not_closed()
 
 
-def tear_down(web_driver, runner):
-    # Close the browser
-    web_driver.quit()
+def test_ultra_fast(eyes, driver):
+    # Navigate to the url we want to test
+    driver.get("https://demo.applitools.com/index_v2.html")
 
-    # we pass false to this method to suppress the exception that is thrown if we
-    # find visual differences
-    all_test_results = runner.get_all_test_results()
-    print(all_test_results)
+    # Call Open on eyes to initialize a test session
+    eyes.open(driver, "Demo App - Python", "Ultrafast grid demo", {"width": 800, "height": 600})
 
+    # check the login page with fluent api, see more info here
+    # https://applitools.com/docs/topics/sdk/the-eyes-sdk-check-fluent-api.html
+    eyes.check("", Target.window().fully().with_name("Login page"))
 
-# Create a new chrome web driver
-web_driver = Chrome(ChromeDriverManager().install())
+    driver.find_element_by_id("log-in").click()
 
-# Create a runner with concurrency of 1
-runner = VisualGridRunner(1)
+    # Check the app page
+    eyes.check("", Target.window().fully().with_name("App page"))
 
-# Create Eyes object with the runner, meaning it'll be a Visual Grid eyes.
-eyes = Eyes(runner)
-
-set_up(eyes)
-
-try:
-    # ⭐️ Note to see visual bugs, run the test using the above URL for the 1st run.
-    # but then change the above URL to https://demo.applitools.com/index_v2.html
-    # (for the 2nd run)
-    ultra_fast_test(web_driver, eyes)
-finally:
-    tear_down(web_driver, runner)
+    # Call Close on eyes to let the server know it should display the results
+    eyes.close(False)
